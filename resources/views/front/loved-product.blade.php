be7b5dc3ebcd5de5c3dc5a07fdcecd5ecb392797
Choirul <choirul@Choiruls-iMac.local>
2017-07-23 21:37:06 +0700

    add quote

--- resources/views/front/loved-product.blade.php
+++ resources/views/front/loved-product.blade.php
@@ -94,7 +94,8 @@
                 <div class="bg-pink box-product2" style="">
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
-                    <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">                    
+                    <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
                     <div class="desc-left">
@@ -112,6 +113,7 @@
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
                     <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
                     <div class="desc-left">
@@ -129,6 +131,7 @@
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
                     <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
                     <div class="desc-left">
@@ -146,6 +149,7 @@
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
                     <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
                     <div class="desc-left">
@@ -163,6 +167,7 @@
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
                     <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
                     <div class="desc-left">
@@ -180,6 +185,7 @@
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
                     <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
                     <div class="desc-left">
@@ -197,6 +203,7 @@
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
                     <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
                     <div class="desc-left">
@@ -214,6 +221,7 @@
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
                     <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
                     <div class="desc-left">
@@ -233,7 +241,7 @@
                 <div class="bg-pink box-product2" style="text-align: center">
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
-                    <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <img class="add-quote corner-right" src="{{asset('front/image/va-dollar.png')}}">
                     <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
@@ -251,7 +259,7 @@
                 <div class="bg-pink box-product2" style="text-align: center">
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
-                    <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <img class="add-quote corner-right" src="{{asset('front/image/va-dollar.png')}}">
                     <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
@@ -269,7 +277,7 @@
                 <div class="bg-pink box-product2" style="text-align: center">
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
-                    <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <img class="add-quote corner-right" src="{{asset('front/image/va-dollar.png')}}">
                     <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
@@ -287,7 +295,7 @@
                 <div class="bg-pink box-product2" style="text-align: center">
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
-                    <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <img class="add-quote corner-right" src="{{asset('front/image/va-dollar.png')}}">
                     <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
@@ -305,7 +313,7 @@
                 <div class="bg-pink box-product2" style="text-align: center">
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
-                    <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <img class="add-quote corner-right" src="{{asset('front/image/va-dollar.png')}}">
                     <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
@@ -323,7 +331,7 @@
                 <div class="bg-pink box-product2" style="text-align: center">
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
-                    <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <img class="add-quote corner-right" src="{{asset('front/image/va-dollar.png')}}">
                     <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
@@ -341,7 +349,7 @@
                 <div class="bg-pink box-product2" style="text-align: center">
                     <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
                     <img class="corner-left" src="{{asset('front/image/va-drop.png')}}">
-                    <img class="corner-right" src="{{asset('front/image/va-dollar.png')}}">
+                    <img class="add-quote corner-right" src="{{asset('front/image/va-dollar.png')}}">
                     <a class="add-compare hidden"><i class="fa fa-plus" style="position: relative;font-size: 10em;top: -160px;"></i></a>
                 </div>
                 <div class="bg-abuabu desc-product">
@@ -393,7 +401,91 @@
         <p class="text-cabin" style="color: black">INGIN MEMASTIKAN KONSUMEN TAU DIMANA BISA MENDAPATKAN PRODUK ANDA?<br>TEMUKAN CARANYA DISINI</p>
     </div>
 </div>
+<!--modal compare-->
+<div class="modal fade" style="padding-top: 10%" id="modalCompare" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document" style="width: 1080px;">
+        <div class="modal-content">
+            <button type="button" class="tutup" data-dismiss="modal" aria-label="Close"><i class="fa fa-times"></i>
+            </button>
+            <div class="modal-body" style="padding-left: 0;padding-right: 0">
+                <div class="row" style="margin-left: 0;margin-right: 0">
+                    <?php $n = 1; ?>
+                    @for($i=0;$i<3;$i++)
+                    <div class="col-md-4 {{($n<3)?'br-1':''}}" style="padding-left: 30px;padding-right: 30px;">
+                        <div class="col-md-12 box-prod-modal">
+                            <!--<div>-->
+                            <div class="bg-pink box-product-modal" style="">
+                                <img class="gambar-full" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
+                            </div>                        
+                        </div>
+                        <div class="col-md-6 pt-20" style="padding-left: 0">
+                            <label class="text-cabin text-16" style="font-weight: bold">Name of Product,</label><br>
+                            <label class="text-cabin text-16" style="font-weight: bold">Brand,</label><br>
+                            <label class="text-cabin text-16" style="font-weight: bold">Store.</label><br>
+                            <p class="pt-30 text-12" style="margin: 0">
+                                Spesification#1,<br>
+                                Spesification#2,<br>
+                                Spesification#3,<br>
+                                Spesification#4
+                            <p class="pt-20 text-12" style="margin: 0">
+                                Price Range<br><br>
+                            </p>
+                        </div>
+                        <div class="col-md-6 pt-20" style="padding-right: 0">
 
+                        </div>
+                        <div class="col-md-12" style="padding-left: 0;padding-right: 0">
+                            <span class="text-cabin" style="float: left;font-size: 10px">
+                                Indonesia, Jakarta, Resor, Furnitur, Aksesori, Tag 6, Tag 7
+                            </span>                            
+                        </div>
+                    </div>
+                    <?php $n++; ?>
+                    @endfor
+                    <div class="col-md-12 mb-20 mt-20">
+                        <button type="button" class="warnadasar no-border text-cabin text-12" style="width: 100%;color: black;height: 40px;font-weight: bold"><i class="fa fa-download"></i>&nbsp;&nbsp;&nbsp;DOWNLOAD AS PDF</button>
+                    </div>
+                </div>
+            </div>            
+        </div>
+    </div>
+</div>
+<!--modal quote-->
+<div class="modal fade" style="padding-top: 10%" id="modalAddQuote" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body" style="padding-left: 0;padding-right: 0">
+                <div class="row" style="margin-left: 30px;margin-right: 30px">
+                    <div class="col-md-12 mb-30" style="font-size: 20px;font-weight: bold">Asking for Price Quote</div>
+                    <div class="col-md-4" style="padding-right: 40px;">
+                        <img class="gambar-full" style="height: 100px;" src="{{asset('front/img/product/1.jpg')}}" alt=""/>
+                    </div>
+                    <div class="col-md-8" style="height: 100px;border-bottom: solid 1px #d1d2d3;padding-left: 0;padding-right: 0;">
+                        <div class="desc-left">
+                            <label class="text-cabin text-14" style="font-weight: bold">Name of Product,</label><br>
+                            <label class="text-cabin text-14" style="font-weight: bold">Brand,</label><br>
+                            <label class="text-cabin text-14" style="font-weight: bold">Store.</label><br>
+                        </div>
+                        <div class="desc-right">
+                            <input type="text" class="text-cabin" style="font-size: 20px;color: #d1d2d3;height: 50px;width: 50px;padding-left: 10px;">
+                            <br>
+                            <label class="text-cabin text-14" style="font-weight: bold;padding-left: 8px">Piece</label>
+                        </div>
+                    </div>                                     
+                    <div class="col-md-offset-4 col-md-8 mb-15" style="padding-left: 0;padding-right: 0;">
+                        <textarea class="text-roboto text-11" style="height: 140px;width: 100%;border: solid 1px #d1d2d3;color: #d1d2d3" placeholder="Pesan Anda di sini :"></textarea>
+                    </div>
+                    <div class="col-md-offset-4 col-md-8 mb-10" style="padding-left: 0;padding-right: 0;">
+                        <button type="button" class="warnadasar no-border text-cabin text-14" style="width: 100%;color: black;height: 40px;font-weight: bold">Minta Penawaran Harga</button>
+                    </div>                    
+                    <div class="col-md-offset-4 col-md-8 mb-30" style="padding-left: 0;padding-right: 0;">
+                        <label class="text-roboto text-14" style="font-weight: lighter">Check bla bla bla bla ....</label><br>
+                    </div>                    
+                </div>
+            </div>            
+        </div>
+    </div>
+</div>
 @endsection
 @section('css')
 <style media="screen" type="text/css">
@@ -435,6 +527,7 @@
         height: 177px;
         width: 100%;
         padding: 0 0 0 0;
+        text-align: center;
     }
     .box-product2 > .corner-left{
         height: 60px;
@@ -487,6 +580,68 @@
         color: blue;
         opacity: 1;
     }
+    //modal  
+    .modal-content {
+        background-color: whitesmoke;
+        text-align: center;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 10%;
+        position: relative;
+        -webkit-box-shadow: 0px 4px 6px 0px rgba(0,0,0,0.1);
+        -moz-box-shadow: 0px 4px 6px 0px rgba(0,0,0,0.1);
+        box-shadow: 0px 4px 6px 0px rgba(0,0,0,0.1);
+    }
+    button.tutup{
+        transition: all 0.5s ease;
+        position: absolute;
+        background-color: #e5c773;
+        right: 0;
+        margin-right: -13px;
+        margin-top: -13px;
+        border-radius: 50%;
+        height: 30px;
+        width: 30px;
+        border: none;
+        color: white;
+        -webkit-box-shadow: -4px -2px 6px 0px rgba(0,0,0,0.1);
+        -moz-box-shadow: -4px -2px 6px 0px rgba(0,0,0,0.1);
+        box-shadow: -3px 1px 6px 0px rgba(0,0,0,0.1);
+    }
+    .box-prod-modal {
+        padding: 0 0 0 0;
+        margin: 3px 0 3px 0;
+    }
+    .box-product-modal {
+        height: 300px;
+        width: 100%;
+        padding: 0 0 0 0;
+    }
+    .box-product-modal > .corner-left{
+        height: 60px;
+        width: 60px;
+        position: relative;
+        margin-top: -310px;
+        /*margin-left: -10px;*/
+        float: left;
+    }
+    .box-product-modal > .corner-right{
+        height: 60px;
+        width: 60px;
+        position: relative;
+        margin-top: -305px;
+        /*margin-left: -10px;*/
+        float: right;
+    }
+    .desc-product-modal {
+        /*        height: 30px;*/
+        padding: 2px 10px 20px 10px;
+        font-family: cabin;
+        font-size: 10px;
+    }
+    .br-1{
+        border-right: 1px solid black;
+    }
 </style>
 @endsection
 @section('js')
@@ -524,7 +679,11 @@
                 $('.add-compare').addClass('hidden');
                 $('.add-compare').removeClass('active');
                 $(this).data('tipe', '0');
+                $('#modalCompare').modal('show');
             }
+        });
+        $('.add-quote').click(function () {
+            $('#modalAddQuote').modal('show');
         });
     });
     function open() {
